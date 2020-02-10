24e2519b37b5be0e83b6b9bb100d68443e1fe7e1
Paolo <fedeli.paolol@gmail.com.com>
2020-02-10 15:35:53 +0100

    Chat React whit Context and socket.io

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,22 @@
 var app = require("express")();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
+const express = require("express");
+const path = require("path");
+const PORT =
+  process.env.PORT || 3001
 
 
-app.get("/", function(req, res) {
-  res.send("<h1>Hello world</h1>");
-});
+app.use(express.static(path.join(__dirname, "../../build")));
+
+app.get("/", (req, res, next) => res.sendFile(__dirname + "./index.html"));
+
+  app.get("/", function(req, res) {
+    res.send("<h1>Hello world</h1>");
+  });
+
+
+
 
 io.on("connection", function (socket) {
   console.log("a user connected");
