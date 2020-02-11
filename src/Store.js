7d9e86aea2d255e1e83396f31d1874842fce79af
Paolo <fedeli.paolol@gmail.com.com>
2020-02-11 16:37:45 +0100

    port env ok

--- src/Store.js
+++ src/Store.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import io from 'socket.io-client'
 import AnimalAvatar from 'animal-avatars.js'
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 
 export const CTX = React.createContext();
